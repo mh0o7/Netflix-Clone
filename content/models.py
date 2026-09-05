90e8a113a0b57f10917763d0d2247735e063ba07
mh0o7 <mehadihasan.rajib10@gmail.com>
2026-09-05 10:11:30 +0600

    Adding Models  some of 4 day recovery

--- content/models.py
+++ content/models.py
@@ -1,20 +1,29 @@
-from enum import unique
 from django.db import models
 
 class Movie(models.Model):
-    tmdb_id = models.interField(unique=True)
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     release_date  = models.DateTimeField()
     description = models.TextField()
-    ratinng = models.FloatField()
-
-
-
-
-
+    rating = models.FloatField()
+    poster_path = models.CharField(max_length=200)
+    backdrop_path = models.CharField(max_length=200)
+    genres = models.JSONField()
+    runtime = models.IntegerField()
+    country = models.CharField(max_length=100)
 
 
 class Series(models.Model):
     title = models.CharField(max_length=100)
+    realease_date = models.DateTimeField()
+    season_number = models.IntegerField()
+    episode_number =  models.IntegerField()
+    episode_name = models.CharField(max_length=100)
+    episode_name = models.CharField(max_length=100)
+    episode_description = models.CharField(max_length=100)
+    rating = models.FloatField()
+    country = models.CharField(max_length=10)
+    
 
+
+    
 
---
+++ content/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Movie, Series
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+class SeriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Series
+        fields = '__all__'
+
+
+
+
